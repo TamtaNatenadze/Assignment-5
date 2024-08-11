72f2d3b5a3ad7d317da1b39f758664d9bd97675c
TamtaNatenadze <tamta.natenadze.1@iliauni.edu.ge>
2024-08-11 07:56:29 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,69 @@
+let temperature = -5;
+console.log(temperature === -5);
+if (temperature < 0) {
+    console.log("ყინავს!");
+} else if (temperature >= 0 && temperature <= 9) {
+    console.log("ძალიან ცივა!");
+} else if (temperature >= 11 && temperature <= 19) {
+    console.log("ცივა!");
+} else if (temperature >= 21 && temperature <= 29) {
+    console.log("თბილა!");
+} else if (temperature > 30) {
+    console.log("ცხელა!");
+}
+
+let temperature1 = 7;
+console.log(temperature1 === 7);
+if (temperature1 < 0) {
+    console.log("ყინავს!");
+} else if (temperature1 >= 0 && temperature1 <= 9) {
+    console.log("ძალიან ცივა!");
+} else if (temperature1 >= 11 && temperature1 <= 19) {
+    console.log("ცივა!");
+} else if (temperature1 >= 21 && temperature1 <= 29) {
+    console.log("თბილა!");
+} else if (temperature1 > 30) {
+    console.log("ცხელა!");
+}
+
+let temperature2 = 19;
+console.log(temperature2 === 19);
+if (temperature2 < 0) {
+    console.log("ყინავს!");
+} else if (temperature2 >= 0 && temperature2 <= 9) {
+    console.log("ძალიან ცივა!");
+} else if (temperature2 >= 11 && temperature2 <= 19) {
+    console.log("ცივა!");
+} else if (temperature2 >= 21 && temperature2 <= 29) {
+    console.log("თბილა!");
+} else if (temperature2 > 30) {
+    console.log("ცხელა!");
+}
+
+let temperature3 = 27;
+console.log(temperature3 === 27);
+if (temperature3 < 0) {
+    console.log("ყინავს!");
+} else if (temperature3 >= 0 && temperature3 <= 9) {
+    console.log("ძალიან ცივა!");
+} else if (temperature3 >= 11 && temperature3 <= 19) {
+    console.log("ცივა!");
+} else if (temperature3 >= 21 && temperature3 <= 29) {
+    console.log("თბილა!");
+} else if (temperature3 > 30) {
+    console.log("ცხელა!");
+}
+
+let temperature4 = 39;
+console.log(temperature4 === 39);
+if (temperature4 < 0) {
+    console.log("ყინავს!");
+} else if (temperature4 >= 0 && temperature4 <= 9) {
+    console.log("ძალიან ცივა!");
+} else if (temperature4 >= 11 && temperature4 <= 19) {
+    console.log("ცივა!");
+} else if (temperature4 >= 21 && temperature4 <= 29) {
+    console.log("თბილა!");
+} else if (temperature4 > 30) {
+    console.log("ცხელა!");
+}
